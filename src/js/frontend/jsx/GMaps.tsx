8d20caf3dee13b8d404a7287ff5389c2c2ab66ec
jollyroger <jollyroger@localhost>
2021-02-28 23:06:10 +0100

    Dynamic markers

--- src/js/frontend/jsx/GMaps.tsx
+++ src/js/frontend/jsx/GMaps.tsx
@@ -1,4 +1,4 @@
-import {GoogleMap, LoadScript, Marker} from '@react-google-maps/api';
+import {GoogleMap, LoadScript} from '@react-google-maps/api';
 import React from "react";
 import {tbkCommonF} from "../../typedefs";
 import {Alert} from '@material-ui/lab';
@@ -15,7 +15,7 @@ export interface GMapsState {
     hidden: boolean
 }
 
-export default class GMaps extends React.Component<GMapsProps, GMapsState> {
+export default class GMaps extends React.PureComponent<GMapsProps, GMapsState> {
     constructor(props: GMapsProps) {
         super(props);
 
@@ -26,6 +26,71 @@ export default class GMaps extends React.Component<GMapsProps, GMapsState> {
             hidden: true
         }
 
+    }
+
+    private map: google.maps.Map;
+    private markers: google.maps.Marker[] = [];
+
+    componentDidUpdate(prevProps: Readonly<GMapsProps>, prevState: Readonly<GMapsState>, snapshot?: any) {
+        this._loadMap();
+    }
+
+    _showMarkers = () => {
+        this._setMapOnAllMarkers(this.map);
+    }
+
+    _setMapOnAllMarkers = (map: google.maps.Map | null) => {
+        for (let i = 0; i < this.markers.length; i++) {
+            this.markers[i].setMap(map);
+        }
+    }
+
+    _clearMarkers = () => {
+        this._setMapOnAllMarkers(null);
+    }
+
+    _deleteMarkers = () => {
+        this._clearMarkers();
+        this.markers = [];
+    }
+
+    _loadMap = () => {
+        const geocoder = new google.maps.Geocoder();
+        const map = this.map;
+        geocoder.geocode({
+            address: this.props.address
+        }, (results, status) => {
+            if (status === google.maps.GeocoderStatus.OK) {
+                map.setCenter(results[0].geometry.location)
+
+                this._deleteMarkers();
+
+                const marker = new google.maps.Marker({
+                    position: results[0].geometry.location,
+                    map,
+                    title   : this.props.address,
+                });
+
+                this.markers.push(marker);
+
+                this.setState({
+                    hidden: false
+                })
+
+            } else if (status === google.maps.GeocoderStatus.REQUEST_DENIED) {
+                this.setState({
+                    error: 'This Google API key is not authorized to use the Geocoding Service.'
+                })
+            } else if (status === google.maps.GeocoderStatus.INVALID_REQUEST || google.maps.GeocoderStatus.ZERO_RESULTS) {
+                this.setState({
+                    error: "The address can't be geocoded by Google."
+                })
+            } else {
+                console.log(status);
+                console.log(results);
+            }
+
+        })
     }
 
 
@@ -42,41 +107,13 @@ export default class GMaps extends React.Component<GMapsProps, GMapsState> {
                 <GoogleMap
                     zoom={this.props.zoom || 16}
                     onLoad={map => {
-                        const geocoder = new google.maps.Geocoder();
-                        geocoder.geocode({
-                            address: this.props.address
-                        }, (results, status) => {
-                            if (status === google.maps.GeocoderStatus.OK) {
-                                map.setCenter(results[0].geometry.location)
-
-                                new google.maps.Marker({
-                                    position: results[0].geometry.location,
-                                    map,
-                                    title   : this.props.address,
-                                });
-
-                                this.setState({
-                                    hidden: false
-                                })
-
-                            } else if (status === google.maps.GeocoderStatus.REQUEST_DENIED) {
-                                this.setState({
-                                    error: 'This Google API key is not authorized to use the Geocoding Service.'
-                                })
-                            } else if (status === google.maps.GeocoderStatus.INVALID_REQUEST || google.maps.GeocoderStatus.ZERO_RESULTS) {
-                                this.setState({
-                                    error: "The address can't be geocoded by Google."
-                                })
-                            } else {
-                                console.log(status);
-                                console.log(results);
-                            }
-
-                        })
+                        this.map = map;
+                        this._loadMap();
                     }}
                     mapContainerStyle={{
-                        height      : this.state.hidden ? '0px' : '260px',
-                        borderRadius: '4px'
+                        height : this.state.hidden ? '0px' : '260px',
+                        border : '1px solid lightgrey',
+                        display: this.state.hidden ? 'none' : 'block',
                     }}
                     options={{
                         mapTypeControl   : false,
