e16dcb6b2e32d2dbba15d1235d0ef1690dbf5d34
jollyroger <jollyroger@localhost>
2021-03-07 23:40:49 +0100

    Added color manipulation helpers

--- VSHM_Framework/Tools.php
+++ VSHM_Framework/Tools.php
@@ -629,6 +629,68 @@ if (!class_exists(Tools::class)) {
                     }
                 }
             }
+
+            return NULL;
+        }
+
+        /**
+         * @param $hexColor
+         *
+         * @return bool
+         */
+        public static function requiresDarkTheme($hexColor)
+        {
+            $R1           = hexdec(substr($hexColor, 1, 2));
+            $G1           = hexdec(substr($hexColor, 3, 2));
+            $B1           = hexdec(substr($hexColor, 5, 2));
+            $blackColor   = "#000000";
+            $R2BlackColor = hexdec(substr($blackColor, 1, 2));
+            $G2BlackColor = hexdec(substr($blackColor, 3, 2));
+            $B2BlackColor = hexdec(substr($blackColor, 5, 2));
+
+            $L1 = 0.2126 * (($R1 / 255) ** 2.2) +
+                0.7152 * (($G1 / 255) ** 2.2) +
+                0.0722 * (($B1 / 255) ** 2.2);
+
+            $L2 = 0.2126 * (($R2BlackColor / 255) ** 2.2) +
+                0.7152 * (($G2BlackColor / 255) ** 2.2) +
+                0.0722 * (($B2BlackColor / 255) ** 2.2);
+
+            if ($L1 > $L2) {
+                $contrastRatio = (int)(($L1 + 0.05) / ($L2 + 0.05));
+            } else {
+                $contrastRatio = (int)(($L2 + 0.05) / ($L1 + 0.05));
+            }
+
+            return !($contrastRatio > 5);
+        }
+
+        /**
+         * Increases or decreases the brightness of a color by a percentage of the current brightness.
+         *
+         * @param string $hexCode       Supported formats: `#FFF`, `#FFFFFF`, `FFF`, `FFFFFF`
+         * @param float  $adjustPercent A number between -1 and 1. E.g. 0.3 = 30% lighter; -0.4 = 40% darker.
+         *
+         * @return  string
+         */
+        public static function adjustBrightness($hexCode, $adjustPercent)
+        {
+            $hexCode = ltrim($hexCode, '#');
+
+            if (strlen($hexCode) === 3) {
+                $hexCode = $hexCode[0] . $hexCode[0] . $hexCode[1] . $hexCode[1] . $hexCode[2] . $hexCode[2];
+            }
+
+            $hexCode = array_map('hexdec', str_split($hexCode, 2));
+
+            foreach ($hexCode as & $color) {
+                $adjustableLimit = $adjustPercent < 0 ? $color : 255 - $color;
+                $adjustAmount    = ceil($adjustableLimit * $adjustPercent);
+
+                $color = str_pad(dechex($color + $adjustAmount), 2, '0', STR_PAD_LEFT);
+            }
+
+            return '#' . implode($hexCode);
         }
 
     }
